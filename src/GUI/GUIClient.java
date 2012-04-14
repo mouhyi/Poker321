b1f9c5b7d05f126f7eb82eca7696a2a9c5912f4b
unpronounceable <yeknom.yknuf+Github@gmail.com>
2012-04-14 18:17:42 -0300

    Update src/GUI/GUIClient.java

--- src/GUI/GUIClient.java
+++ src/GUI/GUIClient.java
@@ -875,7 +875,7 @@ public class GUIClient {
 	 * @author Peter
 	 */
 	public static String[][] retrieveStatistics() throws SQLException {
-		return Server.statisticsModule.LeaderBoard.LeaderBoardDisplay();
+		return Server.statisticsModule.LeaderBoard.leaderBoardDisplay();
 	}
 
 	/**
