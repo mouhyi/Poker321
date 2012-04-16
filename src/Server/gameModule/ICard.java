30add2b0ab92f9a62996b66e2442a53b9e8d6737
Mouhyi <mouhyi.elbouhali@mail.mcgill.ca>
2012-04-16 02:58:45 -0400

    cleaned unused files

--- src/Server/gameModule/ICard.java
+++
@@ -1,9 +0,0 @@
-package Server.gameModule;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ICard extends Remote {
-	public Rank getRank() throws RemoteException;
-	public Suit getSuit() throws RemoteException;
-}
