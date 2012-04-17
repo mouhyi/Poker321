0ddcc50e168c776dd1a04ac1df75cf461bf96e51
Mouhyi <mouhyi.elbouhali@mail.mcgill.ca>
2012-04-16 22:27:41 -0400

    updated DB

--- DB Scripts/team1.sql
+++ DB Scripts/team1.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 16, 2012 at 09:09 AM
+-- Generation Time: Apr 17, 2012 at 04:21 AM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
@@ -17,8 +17,57 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `5CARD`
+-- Database: `team1`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Friends`
+--
+
+CREATE TABLE IF NOT EXISTS `Friends` (
+  `u_id` int(11) NOT NULL,
+  `f_id` int(11) NOT NULL,
+  PRIMARY KEY (`u_id`,`f_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Friends`
+--
+
+INSERT INTO `Friends` (`u_id`, `f_id`) VALUES
+(1, 2),
+(1, 4),
+(2, 1),
+(4, 1),
+(4, 6),
+(5, 6),
+(6, 4),
+(6, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Players`
+--
+
+CREATE TABLE IF NOT EXISTS `Players` (
+  `u_id` int(11) NOT NULL,
+  `wins` int(11) NOT NULL,
+  `losses` int(11) NOT NULL,
+  `gameWinnings` double NOT NULL,
+  PRIMARY KEY (`u_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Players`
+--
+
+INSERT INTO `Players` (`u_id`, `wins`, `losses`, `gameWinnings`) VALUES
+(5, 19, 27, 369),
+(6, 13, 32, -72),
+(8, 0, 1, -20);
 
 -- --------------------------------------------------------
 
@@ -45,8 +94,8 @@ INSERT INTO `Users` (`u_id`, `email`, `password`, `name`, `chips`, `online`) VAL
 (2, 'mouhyi@test.ca', 'qwerty12', 'mouhyi', 100, 0),
 (3, 'test2@test.ca', 'dvsk', 'name', 65, 1),
 (4, 'm@m.m', '0000', 'alves', 490, 1),
-(5, 'q', '2', 'q', 1545, 1),
-(6, '1', '1', '1', 1055, 1),
+(5, 'q', '2', 'q', 1779, 1),
+(6, '1', '1', '1', 979, 1),
 (8, 'a', 'a', 'a', 480, 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
